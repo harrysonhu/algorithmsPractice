e4e214721195d4ee67f02cb3a11d1f4662f1c4f7
harrysonhu <harrysonh95@hotmail.com>
2018-10-23 20:51:41 -0700

    Fix up removeKDigits

--- removeKDigits.js
+++ removeKDigits.js
@@ -1,33 +1,40 @@
 const removeKdigits = function(num, k) {
+	let digitsRemaining = num.length - k;
 	let stack = [];
 	let numParts = num.split('');
-	stack.push(numParts[0]);
 	let top = 0;
-	let pos = 1;
-	while (stack.length > 0 && pos < numParts.length) {
-		if (numParts[pos] < stack[top] && k > 0) {
-			stack.pop();
-      		k--;
-		} else {
-			top++;
+
+	for (let i = 0; i < num.length; i++) {
+		let char = numParts[i];
+		while (top > 0 && stack[top - 1] > char && k > 0) {
+			top--;
+			k--;
 		}
-    	stack.push(numParts[pos]);
-		pos++;
+		stack[top] = char;
+    	top++;
 	}
 
-	let index = null;
-	for (let i = 0; i < stack.length; i++) {
-		if (stack[i] != '0') {
-			index = i;
-			break;
-		}
-	}
-	stack = stack.slice(index);
-	return parseInt(stack.join(''));
+	let index = 0;
+	while (index < stack.length && stack[index] == '0') {
+  		index++;
+  	}
+  	if (index == digitsRemaining) {	
+  		return '0';
+  	} else {
+  		return stack.join('').substring(index, digitsRemaining);
+  	}
 };
+const test0 = removeKdigits('1234567890', 9);
+console.log(test0); // 0
 
 const test1 = removeKdigits('1432219', 3);
-console.log(test1);
+console.log(test1); // 1219
 
-const test2 = removeKdigits('10200', 1);
-console.log(test2);
+const test2 = removeKdigits('112', 1);
+console.log(test2); // 11
+
+const test3 = removeKdigits('100', 1);
+console.log(test3); // 0
+
+const test4 = removeKdigits('10200', 1);
+console.log(test4); // 200
