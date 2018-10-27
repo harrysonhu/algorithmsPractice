0e6261667a2289e4c8627ff59146f411af317efd
harrysonhu <harrysonh95@hotmail.com>
2018-10-26 22:12:48 -0700

    Clear out sortList to practice again

--- sortList.js
+++ sortList.js
@@ -1,39 +1,5 @@
 const sortList = head => {
-	if (head == null || head.next == null) {
-		return head;
-	}
-	let slow = head;
-	let fast = head.next;
-	while (fast != null && fast.next != null) {
-		slow = slow.next;
-		fast = fast.next.next;
-	}
-	let list2 = slow.next;
-	slow.next = null;
-	return merge(sortList(head), sortList(list2));
-};
 
-const merge = (list1, list2) => {
-	let dummy = new ListNode(0);
-	let curr = dummy;
-	while (list1 != null && list2 != null) {
-		if (list1.val < list2.val) {
-			curr.next = list1;
-			list1 = list1.next;
-		} else {
-			curr.next = list2;
-			list2 = list2.next;
-		}
-		curr = curr.next;
-	}
-	if (list1 != null) {
-		curr.next = list1;
-	}
-	if (list2 != null) {
-		curr.next = list2;
-	}
-
-	return dummy.next;
 };
 
 function ListNode(val) {
