a53d159b624dca753cbdfa2b598c6ae485123948
harrysonhu <harrysonh95@hotmail.com>
2018-11-03 12:08:34 -0700

    Imeplement hIndex again to practice

--- findSpecificValue/hIndex.js
+++ findSpecificValue/hIndex.js
@@ -1,5 +1,24 @@
 const hIndex = citations => {
-	
+	let map = new Map();
+	const n = citations.length;
+	for (let i = 0; i < citations.length; i++) {
+		if (citations[i] > n) {
+			map.set(n, (map.get(n) || 0) + 1);
+		} else {
+			map.set(citations[i], (map.get(citations[i]) || 0) + 1);	
+		}
+		
+	}
+	let count = 0;
+	for (let i = n; i >= 0; i--) {
+		if (map.get(i) != null) {
+    		count += map.get(i);
+    	}
+    
+		if (count >= i) {
+			return i;
+		}
+	}
 }
 
 const test1 = hIndex([3, 0, 6, 1, 5]);
