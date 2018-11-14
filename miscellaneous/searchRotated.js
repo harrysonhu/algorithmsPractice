cdabda719342ff565fc0fb60d0445fcebf1d8de3
harrysonhu <harrysonh95@hotmail.com>
2018-11-13 22:03:30 -0800

    Implement searchRotatedArray

--- miscellaneous/searchRotated.js
+++ miscellaneous/searchRotated.js
@@ -1,5 +1,31 @@
 const searchRotated = function(nums, target) {
-	
+	if (nums === null || nums.length === 0) {
+		return -1;
+	}
+	let lo = 0;
+	let hi = nums.length - 1;
+	while (lo <= hi) {
+		let mid = lo + Math.floor((hi - lo) / 2);
+		if (nums[mid] == target) {
+			return mid;
+		}
+		if (nums[lo] <= nums[mid]) {
+			if (nums[lo] <= target && target < nums[mid]) {
+				hi = mid - 1;
+			} else {
+				lo = mid + 1;
+			}
+		}
+		if (nums[mid] <= nums[hi]) {
+			if (nums[mid] < target && target <= nums[hi]) {
+				lo = mid + 1;
+			} else {
+				hi = mid - 1;
+			}
+		}
+	}
+
+	return -1;
 }
 
 const test1 = searchRotated([], 2);
