dc9fd26dfab89718dad6ece30302f7ebe9ee85ee
harrysonhu <harrysonh95@hotmail.com>
2018-11-13 21:07:51 -0800

    Clear out searchRotated to practice again

--- miscellaneous/searchRotated.js
+++ miscellaneous/searchRotated.js
@@ -1,28 +1,5 @@
 const searchRotated = function(nums, target) {
-	let lo = 0;
-	let hi = nums.length - 1;
-	while (lo <= hi) {
-		let mid = Math.floor(lo + (hi - lo) / 2);
-		if (nums[mid] == target) {
-			return mid;
-		}
-		if (nums[lo] <= nums[mid]) {
-			if (target < nums[mid] && target >= nums[lo]) {
-				hi = mid - 1;
-			} else {
-				lo = mid + 1;
-			}
-		}
-
-		if (nums[mid] <= nums[hi]) {
-			if (target > nums[mid] && target <= nums[hi]) {
-				lo = mid + 1;
-			} else {
-				hi = mid - 1;
-			}
-		}
-	}
-	return -1;
+	
 }
 
 const test1 = searchRotated([], 2);
