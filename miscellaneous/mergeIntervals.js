8a1d0c6902c5de6ce1675d6ddc4b8b1f37543bf5
harrysonhu <harrysonh95@hotmail.com>
2018-11-13 20:57:44 -0800

    Implement mergeIntervals again

--- miscellaneous/mergeIntervals.js
+++ miscellaneous/mergeIntervals.js
@@ -1,5 +1,24 @@
 const mergeIntervals = intervals => {
-	
+	if (intervals === null || intervals.length === 0) {
+  		return intervals;
+  	}
+  	const start = [];
+  	const end = [];
+  	for (let i = 0; i < intervals.length; i++) {
+  		start.push(intervals[i][0]);
+    	end.push(intervals[i][1]);
+  	}
+  	start.sort((a, b) => a - b);
+  	end.sort((a, b) => a - b);
+  	const result = [];
+  	let j = 0;
+  	for (let i = 0; i < intervals.length; i++) {
+  		if (i === intervals.length - 1 || start[i + 1] > end[i]) {
+    		result.push([start[j], end[i]]);
+      		j = i + 1;
+    	}
+  	}
+  	return result;
 };
 
 const test1 = mergeIntervals([[1,4], [0, 2], [3, 5]]);
